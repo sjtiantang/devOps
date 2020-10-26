0d9bc6b51a7540da24844ebd995a0bf1dfa18928
sjtiantang <sjtiantang@gmail.com>
2020-10-25 17:42:09 -0700

    change made

--- CTA/main.py
+++ CTA/main.py
@@ -35,19 +35,16 @@ for device_name, device in devices.items():
             time.sleep(sleeptime)
             command_output += host.read_very_eager().decode()
         data = command_output.split('\r\n')
-        if "device status : DOWN" in data[2] or "device status : DOWN" in data[3]:
+        if ": DOWN" in data[2] or ": DOWN" in data[3]:
             if not device_interface_name in penalty_dic:
                 #send email operation
-                penalty_dic[device_interface_name] = 43200
-
-        if device_interface_name in penalty_dic:
-            runtime = datetime.now() - time_start
-            penalty_dic[device_interface_name] -= runtime
-            if penalty_dic[device_interface_name] <= 0:
-                penalty_dic.pop(device_interface_name)
+                penalty_dic[device_interface_name] = True
+        elif device_interface_name in penalty_dic:
+            penalty_dic.pop(device_interface_name)
         #print(data[2])
 
 with open("penalty.json", 'w') as f:
     f.write(json.dumps(penalty_dic))
+
 print("Total runtime: {}".format(datetime.now() - time_start))
 
