4999d566f2f508d685bc9603a4f9f8c2f8be147c
sjtiantang <sjtiantang@gmail.com>
2020-11-08 15:20:27 -0800

    changes made

--- CTA/main.py
+++ CTA/main.py
@@ -3,48 +3,53 @@ from datetime import datetime
 import time
 from auth import username, password
 import json
+from send_email import send_email
 
-time_start = datetime.now()
-sleeptime = 0.27
+while True:
+    time_start = datetime.now()
+    sleeptime = 0.5
 
-with open("devices.json", 'r') as f:
-    devices = json.loads(f.read())
+    with open("devices.json", 'r') as f:
+        devices = json.loads(f.read())
 
-with open("penalty.json", 'r') as f:
-    penalty_dic = json.loads(f.read())
+    with open("penalty.json", 'r') as f:
+        penalty_dic = json.loads(f.read())
 
-for device_name, device in devices.items():
-    print("Operating {} ...".format(device_name))
-    host = telnetlib.Telnet(device["IP"])
-    host.read_until(b"Username:")
-    host.write(username + b'\n')
-    time.sleep(sleeptime)
-    host.write(password + b'\n')
-    time.sleep(sleeptime)
-    host.read_very_eager().decode()
-
-    for interface in device["INTERFACES"]:
-        device_interface_name = device_name + '_' + interface
-        cmd = ("dis int " + interface).encode('utf-8')
-        host.write(b"\n" + cmd + b"\n")
+    for device_name, device in devices.items():
+        print("Operating {} ...".format(device_name))
+        host = telnetlib.Telnet(device["IP"])
+        host.read_until(b"Username:")
+        host.write(username + b'\n')
         time.sleep(sleeptime)
-        
-        command_output = host.read_very_eager().decode()
-        while command_output.endswith("---- More ----"):
-            host.write(b' ')
+        host.write(password + b'\n')
+        time.sleep(sleeptime)
+        host.read_very_eager().decode()
+
+        for interface in device["INTERFACES"]:
+            device_interface_name = device_name + '_' + interface
+            cmd = ("dis int " + interface).encode('utf-8')
+            host.write(b"\n" + cmd + b"\n")
             time.sleep(sleeptime)
-            command_output += host.read_very_eager().decode()
-        data = command_output.split('\r\n')
-        if ": DOWN" in data[2] or ": DOWN" in data[3]:
-            if not device_interface_name in penalty_dic:
-                #send email operation
-                penalty_dic[device_interface_name] = True
-        elif device_interface_name in penalty_dic:
-            penalty_dic.pop(device_interface_name)
-        #print(data[2])
 
-with open("penalty.json", 'w') as f:
-    f.write(json.dumps(penalty_dic))
+            command_output = host.read_very_eager().decode()
+            while command_output.endswith("---- More ----"):
+                host.write(b' ')
+                time.sleep(sleeptime)
+                command_output += host.read_very_eager().decode()
+            data = command_output.split('\r\n')
+            if ": DOWN" in data[2] or ": DOWN" in data[3]:
+                if not device_interface_name in penalty_dic:
+                    send_email([device_name, interface])
+                    penalty_dic[device_interface_name] = True
+            elif device_interface_name in penalty_dic:
+                penalty_dic.pop(device_interface_name)
+            else:
+                print(device_interface_name + "正常")
+            #print(data[2])
 
-print("Total runtime: {}".format(datetime.now() - time_start))
+    with open("penalty.json", 'w') as f:
+        f.write(json.dumps(penalty_dic))
+
+    print("Total runtime: {}".format(datetime.now() - time_start))
+    time.sleep(60)
 
---
+++ CTA/send_email.py
@@ -0,0 +1,38 @@
+import smtplib
+from email.header import Header
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+class MAIL_SENDER():
+    def __init__(self, usermail, password, obj_address, smtpserver, smtpport=25):
+        self.usermail = usermail
+        self.password = password
+        self.obj_address = obj_address
+        self.smtpserver = smtpserver
+        self.smtpport = smtpport
+
+    def create_mail(self, subject, content: list):
+        msg = MIMEMultipart('mixed')
+        msg['From'] = self.usermail
+        msg['To'] = self.obj_address
+        msg['Subject'] = Header(subject, 'utf-8')
+        text = content[0] + ': ' + content[1] + "中断，请尽快查看并处理"
+        text_sub = MIMEText(text, 'plain', 'utf-8')
+        msg.attach(text_sub)
+        return msg.as_string()
+
+    def send(self, msg):
+        try:
+            sender = smtplib.SMTP(self.smtpserver, self.smtpport)
+            sender.login(self.usermail, self.password)
+            sender.sendmail(self.usermail, self.obj_address, msg)
+            print("发送成功")
+            sender.quit()
+        except Exception as e:
+            print("发送失败，错误如下：")
+            print(e)
+
+def send_email(content: list):
+    reporter = MAIL_SENDER("CT_notifier@tom.com", "Cta8889+1", "sjtiantang@gmail.com", "smtp.tom.com")
+    msg = reporter.create_mail("ENNI告警邮件!!!!!", content)
+    reporter.send(msg)
